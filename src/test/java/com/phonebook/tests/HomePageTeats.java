b3289929b65030a954144f9ece255baa5fca2735
derkach2288 <derkach2288@gmail.com>
2024-02-25 18:36:05 +0100

    init commit

---
+++ src/test/java/com/phonebook/tests/HomePageTeats.java
@@ -0,0 +1,21 @@
+package com.phonebook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTeats extends TestBase{
+
+
+    @Test
+    public void isHomeComponentPresentTest () {
+//        System.out.println("Home component is " + isHomeComponentPresent());
+        Assert.assertTrue(isHomeComponentPresent());
+    }
+
+    public boolean isHomeComponentPresent() {
+        return isElementPresent(By.cssSelector("div:nth-child(2)>div>div>h1"));
+
+    }
+
+}
